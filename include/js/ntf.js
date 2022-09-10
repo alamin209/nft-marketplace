92926aec705739500046d29b316aa6fdd267abc8
saiful1420 <saifulislamrazib57@gmail.com>
2022-09-10 23:43:23 +0600

    Home page v2

--- include/js/ntf.js
+++ include/js/ntf.js
@@ -25,3 +25,85 @@ jQuery(document).ready(function($) {
       }
     });
   });
+
+  // owlCarousel trending
+jQuery(document).ready(function($) {
+  "use strict";
+  $('#trending').owlCarousel( {
+      loop: true,
+      center: true,
+      items: 3,
+      margin: 20,
+      autoplay: false,
+      dots:true,
+      nav:true,
+      autoplayTimeout: 8500,
+      smartSpeed: 450,
+      navText: [''],
+      responsive: {
+        0: {
+          items: 1
+        },
+        768: {
+          items: 2
+        },
+        1170: {
+          items: 3
+        }
+      }
+    });
+  });
+    // owlCarousel Hot Item
+jQuery(document).ready(function($) {
+  "use strict";
+  $('#hot_item').owlCarousel( {
+      loop: true,
+      center: true,
+      items: 3,
+      margin: 10,
+      autoplay: false,
+      dots:true,
+      nav:true,
+      autoplayTimeout: 8500,
+      smartSpeed: 450,
+      navText: [''],
+      responsive: {
+        0: {
+          items: 2
+        },
+        768: {
+          items: 2
+        },
+        1170: {
+          items: 3
+        }
+      }
+    });
+  });
+      // owlCarousel Live Auctions
+jQuery(document).ready(function($) {
+  "use strict";
+  $('#live_auctions').owlCarousel( {
+      loop: true,
+      center: true,
+      items: 3,
+      margin: 10,
+      autoplay: false,
+      dots:true,
+      nav:true,
+      autoplayTimeout: 8500,
+      smartSpeed: 450,
+      navText: [''],
+      responsive: {
+        0: {
+          items: 2
+        },
+        768: {
+          items: 2
+        },
+        1170: {
+          items: 3
+        }
+      }
+    });
+  });
